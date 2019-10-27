40741c4974c31eb53eafa69cd6ab861d62694816
Pedermy <pedermyk@gmail.com>
2019-10-27 16:00:11 +0100

    lol

--- arbitrator.py
+++ arbitrator.py
@@ -7,14 +7,14 @@ class Arbitrator:
         self.halt = False
 
     def choose_action(self):
-        best_choice = None
-        for behavior in self.behaviors:
+        best_choice = self.behaviors[0]
+        for behavior in self.behaviors[1:]:
             if behavior.weight > best_choice.weight:
                 best_choice = behavior
 
             elif behavior.weight == best_choice.weight:
                 best_choice = behavior
 
-            return best_choice, False
+            return best_choice
         else:
             return None, True
