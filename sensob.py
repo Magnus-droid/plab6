0b8238319e2e25bf9ba8fbb26b777be04591143e
blund <boergelun@gmail.com>
2019-10-27 16:12:04 +0100

    blblblbl

--- sensob.py
+++ sensob.py
@@ -9,34 +9,38 @@ class Sensor:
         return 0
     
     def update(self) -> None:
-        pass
+        return 0
 
 T = TypeVar('T')
 
 class Sensob(ABC):
     """Container for the different sensor modules"""
+
     def __init__(self, sensors: List[Sensor]):
+        """Initialiser interne varible (sensorer og verdier)"""
         self.sensors = sensors
         self.values = [] # type: List[float]
      
     def update(self):
+        """Oppdater sensorer og lagre verdier"""
         sensor_vals = map(lambda s: s.update, self.sensors) # type: List[T]
         self.values = self.process(sensor_vals)
     
     def get_values(self):
+        """Få prosesserte verdier"""
         return self.values
     
     @abstractmethod
     @staticmethod
     def process(values: List[T]) -> float:
+        """Prosesser input fra ulike sensorer"""
         raise NotImplementedError
         return 0
 
 class FindRed(Sensob):
-    """Returns approximity of red object, 
-    with negative numbers meaning left and 
-    positive meaning right"""
+    """Filtrer ut alt som ikke er rødt fra et bilde"""
 
     @staticmethod
     def process(valies: List[Image]) -> float:
+        """Prosesser bildet og filtrer ut alt som ikke er hvitt"""
         return 0
