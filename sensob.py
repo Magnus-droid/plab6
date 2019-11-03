2a465f670a38b27c274f58e95ac2edd82a2c7d3c
Sondre <swliestol@gmail.com>
2019-11-03 18:46:05 +0100

    Reflect instans.

--- sensob.py
+++ sensob.py
@@ -2,11 +2,13 @@
 
 from PIL import Image, ImageFilter, ImageEnhance
 import ultrasonic
+import reflectance_sensors
+
 
 class Sensor:
     def get_value(self):
         return 0
-    
+
     def update(self):
         return 0
 
@@ -18,36 +20,42 @@ class Sensob:
         """Initialiser interne varible (sensorer og verdier)"""
         self.sensors = sensors
         self.values = []
-     
+
     def update(self):
         """Oppdater sensorer og lagre verdier"""
         sensor_vals = list(map(lambda s: s.update, self.sensors))
         self.values = self.process(sensor_vals)
-    
+
     def get_values(self):
         """Få prosesserte verdier"""
         return self.values
-    
+
     @staticmethod
     def process(values):
         """Prosesser input fra ulike sensorer"""
         raise NotImplementedError
 
-class DistanceSensob(Sensob):
+
+class ReflectanceSensob(Sensob):
+    """Reflectance Sensob"""
+    def __int__(self):
+        reflect = reflectance_sensors.ReflectanceSensors()
+        super().__init__(reflect)
+
     def process(values):
         return values
 
-class ReflectanceSensob(Sensob):
-    def process(values):
-        return values
 
 class DistanceSensob(Sensob):
     """Ultrasonic Senob"""
+
     def __init__(self):
         ultra = ultrasonic.Ultrasonic()
         super().__init__(ultra)
 
     def process(self, values):
         """Returns floating point number"""
-        return values
-
+        for value in values:
+            if value > 0.8:  # if white line
+                return True
+        return False
