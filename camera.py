c503e0f8639d886a81cf0dcb6862dba28ca21080
blund <boergelun@gmail.com>
2019-11-05 16:44:52 +0100

    regret

--- camera.py
+++ camera.py
@@ -7,12 +7,13 @@ import time
 
 class Camera:
 
-    def __init__(self, img_width=128, img_height=96, img_rot=0):
+    def __init__(self, img_width=50, img_height=30, img_rot=0):
         self.value = None
         self.img_width = img_width
         self.img_height = img_height
         self.img_rot = img_rot
         
+        """ 
         subprocess.Popen(["python3", "camera-daemon.py"])
         time.sleep(2) 
 
@@ -22,13 +23,16 @@ class Camera:
         self.s.connect((HOST, PORT))
         print("connected to camera daemon")
         self.s.send(b'ack')
+        """ 
 
-    def get_value(self):  return self.value
+    def get_value(self):  
+        return self.value
 
     def update(self):
         #self.sensor_get_value()
-        self.s.send(b'snap')
-        self.value = Image.open('image.png').convert('RGB')
+        #self.s.send(b'snap')
+        #self.value = Image.open('image.png').convert('RGB')
+        self.value = sensor_get_value()
         return self.value
 
     def reset(self):
@@ -36,7 +40,7 @@ class Camera:
 
     def sensor_get_value(self):
         # This is a OS call that takes a image and makes it accessible to PIL operations in the same directory
-        os.system('raspistill -t 1 --nopreview --exposure sports -o image.png -w "' + str(self.img_width) + '" -h "' + str(self.img_height) + '" -rot "' + str(self.img_rot) + '"')
+        os.system('raspistill -t 1 -o image.png -w "' + str(self.img_width) + '" -h "' + str(self.img_height) + '" -rot "' + str(self.img_rot) + '"')
         # Open the image just taken by raspicam
         # Stores the RGB array in the value field
         self.value = Image.open('image.png').convert('RGB')
