253d357839f453448d0e5d16159daedcaaaa5ff2
Magnus-droid <magnuskrumbacher@gmail.com>
2019-11-05 15:20:07 +0100

    Motor_test

--- motob.py
+++ motob.py
@@ -9,8 +9,8 @@ class Motob:
         self.motor = motor
         self.value = None
         self.instructions = {"R60": (0.4, -0.4), "R30": (0.2, -0.2), "L60": (-0.4, 0.4),
-                             "L30": (-0.2, 0.2), "Backoff": (-0.3, -0.3),
-                             "Forward": (0.3, 0.3), "Turn": (0.5, -0.5)}
+                             "L30": (-0.2, 0.2), "Backoff": (-0.2, -0.2),
+                             "Forward": (0.2, 0.2), "Turn": (0.5, -0.5)}
 
     def update(self, recommendation):
         """Update the information for recommended action"""
